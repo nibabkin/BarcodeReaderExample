db2f8fff6105e4b6d41e483c1da5c8877b7d4635
Nikita Babkin <nibabkin@gmail.com>
2017-02-17 17:56:07 +0300

    Fabric integrated

---
+++ app/src/main/java/ru/teamidea/barcodereader/App.java
@@ -0,0 +1,20 @@
+package ru.teamidea.barcodereader;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by nbabkin on 17.02.17.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Fabric.with(this, new Crashlytics());
+    }
+}
