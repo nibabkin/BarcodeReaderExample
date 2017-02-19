ad7fe3ea530c02f88890b2dec92bb924940219bd
Nikita Babkin <nibabkin@gmail.com>
2017-02-19 23:23:22 +0300

    UI updates 3

--- app/src/main/java/ru/teamidea/barcodereader/ui/BarcodeCaptureActivity.java
+++ app/src/main/java/ru/teamidea/barcodereader/ui/BarcodeCaptureActivity.java
@@ -1,7 +1,14 @@
 package ru.teamidea.barcodereader.ui;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import ru.teamidea.barcodereader.R;
 
 public class BarcodeCaptureActivity extends CaptureActivity {
 
+    protected DecoratedBarcodeView initializeContent() {
+        setContentView(R.layout.activity_barcode_capture);
+        return (DecoratedBarcodeView)findViewById(R.id.zxing_barcode_scanner);
+    }
 }
